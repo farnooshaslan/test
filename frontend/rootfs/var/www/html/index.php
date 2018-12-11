ec2607cb9781310804d28cda821930db9fad1ceb
Jacopo Nardiello <jacopo@sighup.io>
2018-12-11 16:59:55 +0100

    Improved layout

--- frontend/rootfs/var/www/html/index.php
+++ frontend/rootfs/var/www/html/index.php
@@ -39,39 +39,77 @@ $backend_tasks = json_decode(file_get_contents("http://${backend_url}/tasks"), t
 <!DOCTYPE html>
 <html>
     <head>
-        <title>KubePrimer PowerApp</title>
+        <title>PowerApp - Your Awesome List App</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
+        <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
     </head>
-    
     <body>
-        <!-- Intro -->
-        <div id="intro">
-            <h1>KubePrimer PowerApp</h1>
-            <p>With this app you will be able to create a very simple list of items.</p>
-            <p>This page was served by <?php echo $hostname ?></p>
-        </div>
+        <section class="section">
+            <!-- <div class="hero-body"> -->
+                <div class="container">
+                    <h1 class="title">PowerApp</h1>
+                    <h2 class="subtitle">Your Awesome List App</h2>
+                </div>
+            <!-- </div> -->
+        </section>
+        <section class="section">
+        <div class="container">
+            <div class="columns">
+                <div class="column">
+                    <div class="columns">
+                        <div class="column">
+                            <!-- Intro -->
+                            <!-- <div id="intro">
+                                <p>This page was served by <?php echo $hostname ?></p>
+                            </div> -->
 
-        <!-- Form -->
-        <div id="form">
-            <form action="index.php" method="post">
-                <input type="text" name="task">
-                <input type="submit">
-            </form>
+                            <!-- Form -->
+                            <div id="form">
+                                <form action="index.php" method="post">
+                                    <p>Add new a item here:</p>
+                                    <div class="field">
+                                        <div class="control">
+                                            <input class="input is-small" type="text" name="task"><br />
+                                        </div>
+                                    </div>
+                                    <div class="field">
+                                        <div class="control">
+                                            <input class="button is-link" type="submit">
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="column">
+                            <!-- List -->
+                            <div id="list">
+                                <table class="table is-hoverable is-bordered is-fullwidth is-striped">
+                                <?php
+                                    if (!empty($backend_tasks['tasks'])) {
+                                        foreach ($backend_tasks['tasks'] as $key => $value) {
+                                            echo "<tr><td>${value['task']}</td><td><a href=\"index.php?delete=${value['_id']}\">delete</a></td></tr>";
+                                        }
+                                    } else {
+                                        echo "<tr><td>No data to show, yet!</tr></td>";
+                                    }
+                                ?>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <table class="table is-bordered is-striped">
+                        <tr class="is-uppercase has-text-weight-bold"><td>Container Data</td><td></td></tr>
+                        <tr><td>HostName</td><td><?php echo $hostname ?></td></tr>
+                    </table>
+                </div>
+                <div class="column">
+                    <figure class="image is-16by9">
+                        <a href="https://sighup.io"><img src="images/sighup.png" /></a>
+                    <figure>
+                </div>
+            </div>
         </div>
-
-        <!-- List -->
-        <div id="list">
-            <table>
-            <?php
-                if (!empty($backend_tasks['tasks'])) {
-                    foreach ($backend_tasks['tasks'] as $key => $value) {
-                        echo "<tr><td>${value['task']}</td><td><a href=\"index.php?delete=${value['_id']}\">delete</a></td></tr>";
-                    }
-                } else {
-                    echo "<tr><td>No data to show, yet!</tr></td>";
-                }
-            ?>
-            </table>
-        </div>
+        </section>
     </body>
 </html>
 
